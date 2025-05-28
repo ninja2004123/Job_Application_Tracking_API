635259b31245f2740273c57e5f6c94c05e016b90
Oogway404 <nikolakostadinov2004@gmail.com>
2025-05-28 23:28:12 +0200

    Final Project Sumbission

---
+++ src/main/java/Final_Project_5806/Final_Project/JobApplicationTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package Final_Project_5806.Final_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobApplicationTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobApplicationTrackingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Final_Project_5806/Final_Project/JobApplicationTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package Final_Project_5806.Final_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
